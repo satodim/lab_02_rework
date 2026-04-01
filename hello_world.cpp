a34562688e3090be863424752eaa384e55d35066
satodim <vovkakursk8@gmail.com>
2026-04-01 06:20:37 +0000

    changed code style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,14 @@
 // including standard IO library
 #include <iostream>
 
-int main() {
-	// creating string for name
-	std::string name;
-	// recieving name
-	std::cin >> name;
-	// printing "hello world from name"
-	std::cout << "Hello world from " << name << std::endl;
-	return 0;
+int
+main()
+{
+  // creating string for name
+  std::string name;
+  // recieving name
+  std::cin >> name;
+  // printing "hello world from name"
+  std::cout << "Hello world from " << name << std::endl;
+  return 0;
 }
